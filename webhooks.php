927945114c35ec94da032a8faf6357561248db3d
illusterpc <44196200+illusterpc@users.noreply.github.com>
2018-10-16 17:28:41 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'hD6RnM6TB4izipj4cZtbcRxvWL7nuskhpK7u5+eHzPxZ+/1MDThXpfIkxoc+Z4+9uxuwzevWJltw7eektxMFMX1uMRIyi44vfPf/FTJs3ICqAyRPdAKXkubGmEzy0dVBlCRyXw/QycNsCxtIinTGxQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
